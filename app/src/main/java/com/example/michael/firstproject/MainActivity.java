681f5a451b073ba04915df3db42bc0e02f144f1b
Biz <bizzo174@gmail.com>
2017-03-31 14:16:31 +0200

    landscape constraints

--- app/src/main/java/com/example/michael/firstproject/MainActivity.java
+++ app/src/main/java/com/example/michael/firstproject/MainActivity.java
@@ -3,23 +3,23 @@ package com.example.michael.firstproject;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.*;
 import android.view.*;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    int counterTextSize = 0;
     int op1 = -1;
     int op2 = -1;
     float result;
     char operation;
+    boolean flagResult = false;
 
     ScrollView scrollResult;
 
-    TextView onGoingOperation,
-            risultato;
+    TextView operationHistory,
+            currentNumber,
+            currentOperation;
 
     Button bottonePiu,
             bottoneMen,
@@ -41,32 +41,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
         scrollResult = (ScrollView) findViewById(R.id.scrollResult);
 
-        onGoingOperation = (TextView) findViewById((R.id.onGoingOperation));
-        risultato = (TextView) findViewById(R.id.risultato);
+        operationHistory = (TextView) findViewById((R.id.operationHistory));
+        currentNumber = (TextView) findViewById(R.id.currentNumber);
+        currentOperation = (TextView) findViewById(R.id.currentOperation);
 
-        bottone0 = (Button) findViewById(R.id.bottone0);
+        currentOperation.setOnClickListener(this);
+
+        bottone0 = (Button) findViewById(R.id.numPad0);
         bottone0.setOnClickListener(this);
-        bottone1 = (Button) findViewById(R.id.bottone1);
+        bottone1 = (Button) findViewById(R.id.numPad1);
         bottone1.setOnClickListener(this);
-        bottone2 = (Button) findViewById(R.id.bottone2);
+        bottone2 = (Button) findViewById(R.id.numPad2);
         bottone2.setOnClickListener(this);
-        bottone3 = (Button) findViewById(R.id.bottone3);
+        bottone3 = (Button) findViewById(R.id.numPad3);
         bottone3.setOnClickListener(this);
-        bottone4 = (Button) findViewById(R.id.bottone4);
+        bottone4 = (Button) findViewById(R.id.numPad4);
         bottone4.setOnClickListener(this);
-        bottone5 = (Button) findViewById(R.id.bottone5);
+        bottone5 = (Button) findViewById(R.id.numPad5);
         bottone5.setOnClickListener(this);
-        bottone6 = (Button) findViewById(R.id.bottone6);
+        bottone6 = (Button) findViewById(R.id.numPad6);
         bottone6.setOnClickListener(this);
-        bottone7 = (Button) findViewById(R.id.bottone7);
+        bottone7 = (Button) findViewById(R.id.numPad7);
         bottone7.setOnClickListener(this);
-        bottone8 = (Button) findViewById(R.id.bottone8);
+        bottone8 = (Button) findViewById(R.id.numPad8);
         bottone8.setOnClickListener(this);
-        bottone9 = (Button) findViewById(R.id.bottone9);
+        bottone9 = (Button) findViewById(R.id.numPad9);
         bottone9.setOnClickListener(this);
 
         bottonePiu = (Button) findViewById(R.id.bottonePiu);
@@ -85,14 +88,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bottoneDel.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                onGoingOperation.setText("");
-                risultato.setText("");
+                operationHistory.setText("");
+                currentNumber.setText("");
+                currentOperation.setText("");
                 op1 = -1;
                 op2 = -1;
-
+                enableOperationButton(false);
                 return true;
             }
         });
+
+        enableOperationButton(false);
 
     }
 
@@ -100,103 +106,113 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
 
-            case R.id.bottone0:
-                risultato.append("0");
+            case R.id.currentOperation:
+                if(flagResult == true) checkCurrentOperation();
                 break;
 
-            case R.id.bottone1:
-                risultato.append("1");
+            case R.id.numPad0:
+                numPadFunction("0");
+
                 break;
 
-            case R.id.bottone2:
-                risultato.append("2");
+            case R.id.numPad1:
+                numPadFunction("1");
                 break;
 
-            case R.id.bottone3:
-                risultato.append("3");
+            case R.id.numPad2:
+                numPadFunction("2");
                 break;
 
-            case R.id.bottone4:
-                risultato.append("4");
+            case R.id.numPad3:
+                numPadFunction("3");
                 break;
 
-            case R.id.bottone5:
-                risultato.append("5");
+            case R.id.numPad4:
+                numPadFunction("4");
                 break;
 
-            case R.id.bottone6:
-                risultato.append("6");
+            case R.id.numPad5:
+                numPadFunction("5");
                 break;
 
-            case R.id.bottone7:
-                risultato.append("7");
+            case R.id.numPad6:
+                numPadFunction("6");
                 break;
 
-            case R.id.bottone8:
-                risultato.append("8");
+            case R.id.numPad7:
+                numPadFunction("7");
                 break;
 
-            case R.id.bottone9:
-                risultato.append("9");
+            case R.id.numPad8:
+                numPadFunction("8");
+                break;
+
+            case R.id.numPad9:
+                numPadFunction("9");
                 break;
 
             case R.id.bottonePiu:
-                op1 = Integer.parseInt(risultato.getText().toString());
-                risultato.setText("");
-                onGoingOperation.append("\n" + op1 + "+");
+                op1 = Integer.parseInt(currentNumber.getText().toString());
+                currentNumber.setText("");
+                currentOperation.append(op1 + "+");
                 operation = '+';
+                enableOperationButton(false);
                 break;
 
             case R.id.bottoneMen:
-                op1 = Integer.parseInt(risultato.getText().toString());
-                risultato.setText("");
-                onGoingOperation.append("\n" + op1 + "-");
+                op1 = Integer.parseInt(currentNumber.getText().toString());
+                currentNumber.setText("");
+                currentOperation.append(op1 + "-");
                 operation = '-';
+                enableOperationButton(false);
                 break;
 
             case R.id.bottonePer:
-                op1 = Integer.parseInt(risultato.getText().toString());
-                risultato.setText("");
-                onGoingOperation.append("\n" + op1 + "*");
+                op1 = Integer.parseInt(currentNumber.getText().toString());
+                currentNumber.setText("");
+                currentOperation.append(op1 + "*");
                 operation = '*';
+                enableOperationButton(false);
                 break;
 
             case R.id.bottoneDiv:
-                op1 = Integer.parseInt(risultato.getText().toString());
-                risultato.setText("");
-                onGoingOperation.append("\n" + op1 + "/");
+                op1 = Integer.parseInt(currentNumber.getText().toString());
+                currentNumber.setText("");
+                currentOperation.append(op1 + "/");
                 operation = '/';
+                enableOperationButton(false);
                 break;
 
             case R.id.bottoneDel:
-                risultato.setText("");
+                currentNumber.setText("");
                 break;
 
             case R.id.bottoneUgu:
                 if (op1 == -1) {
                     setToast("EH VOLEVII");
+                    enableOperationButton(false);
                     break;
                 }
-                op2 = Integer.parseInt(risultato.getText().toString());
-                risultato.setText("");
-                onGoingOperation.append("" + op2);
+                op2 = Integer.parseInt(currentNumber.getText().toString());
+                currentNumber.setText("");
+                currentOperation.append("" + op2);
                 switch (operation) {
 
                     case '+':
                         result = op1 + op2;
-                        onGoingOperation.append(" = " + result);
+                        currentOperation.append(" = " + result);
                         scrollResult.fullScroll(ScrollView.FOCUS_DOWN);
                         break;
 
                     case '-':
                         result = op1 - op2;
-                        onGoingOperation.append(" = " + result);
+                        currentOperation.append(" = " + result);
                         scrollResult.fullScroll(ScrollView.FOCUS_DOWN);
                         break;
 
                     case '*':
                         result = op1 * op2;
-                        onGoingOperation.append(" = " + result);
+                        currentOperation.append(" = " + result);
                         scrollResult.fullScroll(ScrollView.FOCUS_DOWN);
                         break;
 
@@ -207,7 +223,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             break;
                         }
                         result = op1 / (float) op2;
-                        onGoingOperation.append(" = " + result);
+                        currentOperation.append(" = " + result);
                         scrollResult.fullScroll(ScrollView.FOCUS_DOWN);
                         break;
 
@@ -215,7 +231,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         break;
 
                 }
+                flagResult = true;
+                enableOperationButton(false);
                 scrollResult.fullScroll(ScrollView.FOCUS_DOWN);
+
                 break;
 
             default:
@@ -231,6 +250,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    /*
+        Funzione invocata quando un bottone numPad è premuto
+     */
+    public void numPadFunction(String buttonValue){
+        if (flagResult == true) checkCurrentOperation();
+        currentNumber.append(buttonValue);
+        enableOperationButton(true);
+    }
+
+    /*
+        Funzione che pulisce la TextView currentOperation
+     */
+    public void checkCurrentOperation(){
+            flagResult = false;
+            operationHistory.append("\n" + currentOperation.getText().toString());
+            currentOperation.setText("");
+    }
+
+    /*
+        Funzione che gestisce l'attivazione delle operazioni
+     */
+    public void enableOperationButton(boolean flag){
+        if (flag == false){
+            bottonePiu.setEnabled(false);
+            bottoneMen.setEnabled(false);
+            bottonePer.setEnabled(false);
+            bottoneDiv.setEnabled(false);
+            bottoneUgu.setEnabled(false);
+        }else{
+            bottonePiu.setEnabled(true);
+            bottoneMen.setEnabled(true);
+            bottonePer.setEnabled(true);
+            bottoneDiv.setEnabled(true);
+            bottoneUgu.setEnabled(true);
+        }
     }
 
 }
